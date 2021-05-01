cf9bc387b0d1bde8cc842f54e0008f34ba1f704e
Sukhbir Singh <sukhbir2388@gmail.com>
2021-05-01 18:00:16 +0530

    onResponse method added

--- ABCGAMC/app/src/main/java/com/abc/MainActivity.kt
+++ ABCGAMC/app/src/main/java/com/abc/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.abc
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onResponse(data: Any) {
+
     }
 }
